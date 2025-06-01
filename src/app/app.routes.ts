e74ed980b1e2eb9c499c30a23714e1084b264768
c-clarke institute <c-clarke@c-clarkes-Mac-mini.local>
2025-06-01 12:28:22 +0530

    Data binding examples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: 'home' , component :HomeComponent},
-    {path: 'product', component:ProductComponent}
+    {path: 'product', component:ProductComponent},
+    {path: 'about', component:AboutComponent}
 ];
