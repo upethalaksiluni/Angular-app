6f24bf5971d8534193c98b49b77f4caf3c231b5c
upethalaksiluni <upethalaksiluni@gmail.com>
2025-06-08 10:49:10 +0530

    add one more product

--- src/app/product-container/product-container.component.ts
+++ src/app/product-container/product-container.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductComponent } from "../product/product.component";
+import { Product } from '../modals/product';
 
 @Component({
   selector: 'app-product-container',
@@ -8,5 +9,19 @@ import { ProductComponent } from "../product/product.component";
   styleUrl: './product-container.component.scss'
 })
 export class ProductContainerComponent {
+  chocolatecake: Product = {
+      productName: 'Chocoloate Cake',
+      price: 100,
+      stockCount: 2,
+      imgUrl: 'https://www.shutterstock.com/image-photo/chocolate-cake-berries-260nw-394680466.jpg',
+      description: 'A rich, moist, and decadent chocolate cake layered with velvety chocolate ganache and topped with a smooth, creamy frosting. Made with premium cocoa and fresh ingredients, this indulgent dessert melts in your mouth with every bite. Perfect for chocolate lovers and special occasions alike.'
+    }
 
+  vanilacake: Product = {
+    productName: 'Vanila Cake',
+    price: 100,
+    stockCount: 2,
+    imgUrl: 'https://imgs.search.brave.com/5f1S26kRaqvPBfCywP528mueTLYS2dxbM6wRp399Pco/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdj/ZG4uZmxvd2VyYXVy/YS5jb20vRFNDXzgw/MTkuanBn',
+    description: 'A rich, moist, and decadent chocolate cake layered with velvety chocolate ganache and topped with a smooth, creamy frosting. Made with premium cocoa and fresh ingredients, this indulgent dessert melts in your mouth with every bite. Perfect for chocolate lovers and special occasions alike.'
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../modals/product';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,13 +13,7 @@ import { RedHighlightDirective } from '../directives/red-highlight.directive';
 })
 export class ProductComponent {
 
-    product: Product = {
-      productName: 'Chocoloate Cake',
-      price: 100,
-      stockCount: 2,
-      imgUrl: 'https://www.shutterstock.com/image-photo/chocolate-cake-berries-260nw-394680466.jpg',
-      description: 'A rich, moist, and decadent chocolate cake layered with velvety chocolate ganache and topped with a smooth, creamy frosting. Made with premium cocoa and fresh ingredients, this indulgent dessert melts in your mouth with every bite. Perfect for chocolate lovers and special occasions alike.'
-    }
+    @Input() product!: Product;
   
 
 }
