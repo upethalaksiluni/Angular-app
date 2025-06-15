dfcba4d943c87c1592ab2757a5606226a63a74f8
upethalaksiluni <upethalaksiluni@gmail.com>
2025-06-15 11:58:26 +0530

    shopping cart created

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  count = signal<number>(0);
+  constructor() { }
+}
---
+++ src/app/services/logger.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class LoggerService{
+    log(message: string): void{
+        console.log('LoggerService: ${message}')
+    }
+}
